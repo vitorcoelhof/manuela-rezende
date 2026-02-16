4dd3db7be6bdf02280917901793866449229f7d0
Vitor <vitorcoelhof@gmail.com>
2026-02-16 07:14:15 -0300

    fix(01-01): correct SanityImageSource import path for @sanity/image-url v2

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,5 +1,5 @@
 import imageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import { client } from './client'
 
 const builder = imageUrlBuilder(client)
