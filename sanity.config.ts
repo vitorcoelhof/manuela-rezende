1d8849c6914506b04da802e27d98857a8cf579db
Vitor <vitorcoelhof@gmail.com>
2026-03-07 13:50:23 -0300

    fix: simplificar estrutura do Studio removendo customização problemática

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,24 +3,7 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemaTypes'
 
-const plugins = [
-  structureTool({
-    structure: (S) =>
-      S.list()
-        .title('Conteúdo')
-        .items([
-          S.listItem()
-            .title('Imóveis')
-            .child(S.documentTypeList('imovel')),
-          S.listItem()
-            .title('Corretora')
-            .child(S.documentTypeList('corretora')),
-          S.listItem()
-            .title('Consultas')
-            .child(S.documentTypeList('consulta')),
-        ]),
-  }),
-]
+const plugins = [structureTool()]
 
 // Vision tool (debug/exploration) only enabled in development
 if (process.env.NODE_ENV === 'development') {
